f767fa0c8984f8a9249616c9c03add19f2aca05a
ka-lucas <katarinelucas1@gmail.com>
2025-05-11 21:01:03 -0300

    infra: add Terraform configuration for Cloud Run deployment

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  name         = "devops-459516-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
