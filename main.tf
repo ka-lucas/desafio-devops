06cfd76baf89d1c0435399b3542367273cd32ba3
ka-lucas <katarinelucas1@gmail.com>
2025-05-13 21:31:17 -0300

    infra: rename VM to avoid conflict during Terraform apply

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "devops-459516-vm"
+  name         = "devops-459516-vm-ci"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
 
