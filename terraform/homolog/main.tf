66cea0c39c34707c9de58abc8fcd8339b776bbfe
ka-lucas <katarinelucas1@gmail.com>
2025-05-13 21:47:37 -0300

    infra: add Cloud Run deploy configs for homolog and prod

---
+++ terraform/homolog/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "devops-459516"
+  region  = "us-central1-a"
+}
+
+resource "google_cloud_run_service" "homolog" {
+  name     = "desafio-api-homolog"
+  location = "us-central1-a"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/devops-459516/desafio-api"
+        ports {
+          container_port = 8080
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "noauth" {
+  service  = google_cloud_run_service.homolog.name
+  location = google_cloud_run_service.homolog.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
